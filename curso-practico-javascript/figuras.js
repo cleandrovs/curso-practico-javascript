0137988c8d46ebfa184320ed11c19366e1191ba1
cleandrovs <cleandrovs@gmail.com>
2021-10-05 11:50:25 -0500

    primer commit

---
+++ curso-practico-javascript/figuras.js
@@ -0,0 +1,12 @@
+//código del cuadrado
+const ladoCuadrado = 5;
+console.log('los lados del cuadrado son: '+ladoCuadrado+'cm');
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log('el Perimetro del cuadrado es: ' + perimetroCuadrado+'cm');
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+
+console.log('El área del Cuadrado es: '+ areaCuadrado+'cm2');
+//código del triangulo
