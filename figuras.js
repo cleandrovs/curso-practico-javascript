4349777484b4cfcdb41f41e28d32728fdaa58bcc
cleandrovs <cleandrovs@gmail.com>
2021-10-05 10:16:53 -0500

    figuras geometricas

---
+++ figuras.js
@@ -0,0 +1,10 @@
+const ladoCuadrado = 5;
+console.log('los lados del cuadrado son: '+ladoCuadrado+'cm');
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log('el Perimetro del cuadrado es: ' + perimetroCuadrado+'cm');
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+
+console.log('El área del Cuadrado es: '+ areaCuadrado+'cm2');
