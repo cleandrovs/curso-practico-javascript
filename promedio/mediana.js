fcdb69ddb68169a3f7cf6d90f15fe283e67e4f72
cleandrovs <cleandrovs@gmail.com>
2021-10-12 13:07:29 -0500

    se agregar para calcular la mediana desde un input

--- promedio/mediana.js
+++ promedio/mediana.js
@@ -6,44 +6,58 @@ const lista1 = [
     500
 ];
 
-const lista1Ordenada = lista1.sort((a,b)=> a-b);
-console.log(lista1Ordenada); 
+var Lista =[];
+function agregarDato(){
+  const input = Number( document.getElementById('datoArray').value);
+  var listaNueva= Lista.push(input);
+  var resultadoLista = document.getElementById('resultadoLista');
+  resultadoLista.innerText='Los datos ingresados son: ' + Lista+ ' si es correcto proceda a calclular la media aritmerica';
 
-function calcularMediaaritmetica(lista) {
+  const lista1Ordenada = Lista.sort((a,b)=> a-b);
+  var listaOrdenada = document.getElementById('listaOrdenada');
+  listaOrdenada.innerText='Los datos ordenados son los siguientes: '+ lista1Ordenada;
+}
+
+function calcularMediana(){
+  function calcularMediaaritmetica() {
     // let sumaLista = 0;
     // for (let i = 0; i < lista.length; i++) {
     //   sumaLista = sumaLista + lista[i];
     // }
   
-    const sumaLista = lista.reduce(
+    const sumaLista = Lista.reduce(
       function (valorAcumulado = 0, nuevoElemento) {
         return valorAcumulado + nuevoElemento;
       }
     );
   
-    const promedioLista = sumaLista / lista.length;
+    const promedioLista = sumaLista / Lista.length;
   
     return promedioLista;
   }
 
-const mitaLista1 = parseInt(lista1.length/2);
+  const mitaLista1 = parseInt(Lista.length/2);
 
-let mediana;
+  var resultadoMediana = document.getElementById('resultadoMediana');
+  let mediana;
 
-function esPar(numerito){
+  function esPar(numerito){
     if(numerito % 2===0){
         return true;
     }else{
         return false;
     }
-}
-if (esPar(lista1)){
-const elemento1= lista1Ordenada[mitaLista1];
-const elemento2= lista1Ordenada[mitaLista1-1]
+  }
+  if (esPar(Lista)){
+  const elemento1= lista1Ordenada[mitaLista1];
+  const elemento2= lista1Ordenada[mitaLista1-1]
 
-const promedioelemento1y2 = calcularMediaaritmetica([elemento1,elemento2])
+  const promedioelemento1y2 = calcularMediaaritmetica([elemento1,elemento2])
 
-mediana = promedioelemento1y2;
-}else{
- mediana=lista1[mitaLista1];
+  mediana = promedioelemento1y2;
+    resultadoMediana.innerText='La mediana es: '+mediana;
+  }else{
+  mediana=Lista[mitaLista1];
+    resultadoMediana.innerText= 'la mediana es: '+mediana;
+  }
 }
